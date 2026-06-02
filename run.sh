c37a48356be82ef867544b3c77527b8beb5dce41
luizing <luizeduardo2099@gmail.com>
2026-06-02 19:31:39 -0300

    versao bash dos scripts

---
+++ run.sh
@@ -0,0 +1,10 @@
+./cleanup.sh
+
+flex="lib/jflex-full-1.9.1.jar"
+cup="lib/java-cup-11b.jar"
+libs=".:lib/java-cup-11b.jar:lib/java-cup-11b-runtime.jar:lib/jflex-1.8.2.jar"
+
+java -jar "$flex" Rascunho.flex
+java -jar "$cup" -parser parser -symbols sym Rascunho.cup
+javac -cp "$libs" *.java
+java -cp "$libs" Main
