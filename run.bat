d454e7eeb367c8b92f97bb852566ff27429ea428
Luiz <luizeduardo2099@gmail.com>
2026-06-06 23:29:13 -0300

    corrigir run.bat

--- run.bat
+++ run.bat
@@ -6,6 +6,6 @@ set "cup=lib\java-cup-11b.jar"
 set "libs=.;lib\java-cup-11b.jar;lib\java-cup-11b-runtime.jar;lib\jflex-1.8.2.jar"
 
 java -jar %flex% LIP.flex
-java -jar %cup% -parser parser -symbols sym LIP.cup
+java -jar %cup% -expect 1 -parser parser -symbols sym LIP.cup
 javac -cp "%libs%" *.java
 java -cp "%libs%" Main input.txt
